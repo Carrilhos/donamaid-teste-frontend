172b9a05535f3bb70d572d4f3ab3624a36ddcfd7
Carrilhos <gcarrilhos@gmail.com>
2020-07-24 16:05:45 -0300

    build

---
+++ donamaid/build/precache-manifest.aad9258840d6b3abfea8459ed9ee3956.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "87ae98a20db167b607be6209495ffd41",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6d4780f58b8e1b4bd555",
+    "url": "/static/css/main.2df874de.chunk.css"
+  },
+  {
+    "revision": "c33cea78eac9ddd41be8",
+    "url": "/static/js/2.6f5c96ca.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.6f5c96ca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6d4780f58b8e1b4bd555",
+    "url": "/static/js/main.313f0caf.chunk.js"
+  },
+  {
+    "revision": "9832dd9b2d88ae95645c",
+    "url": "/static/js/runtime-main.8b9fcee4.js"
+  },
+  {
+    "revision": "5cfe0daf8b706dbe422baa2b8cd56090",
+    "url": "/static/media/donamaid-logo.5cfe0daf.svg"
+  },
+  {
+    "revision": "6701a435a17939d9cb3bd7bff453c912",
+    "url": "/static/media/donie.6701a435.svg"
+  },
+  {
+    "revision": "6ff69fd1d653c114e9b96a051e46a4d7",
+    "url": "/static/media/frajola.6ff69fd1.png"
+  },
+  {
+    "revision": "62266bb82f57d29e3d27be67b3f2b484",
+    "url": "/static/media/horizontal-branco.62266bb8.png"
+  }
+]);
